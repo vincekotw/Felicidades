3be15e01fa5ae06aea472ada62826e348b912fdf
vincekotw <156933306+vincekotw@users.noreply.github.com>
2024-09-24 17:58:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('openButton').addEventListener('click', function() {
+    document.getElementById('insideCard').style.display = 'block';
+});
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('insideCard').style.display = 'none';
+});
